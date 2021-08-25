97a93ae518e48b297ba2cac7cc8735fb09296b57
David López <39871533+buronsuave@users.noreply.github.com>
2021-08-24 19:29:47 -0500

    Delete sistemas-distribuidos/middleware/external directory

--- sistemas-distribuidos/middleware/external/main.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = require("./routes/routes");
-const bodyParser = require('body-parser')
-
-const app = express();
-app.use(bodyParser.json());
-app.use("/", router);
-app.listen(8080, () => console.log("Server is running on http//localhost:8080"));
